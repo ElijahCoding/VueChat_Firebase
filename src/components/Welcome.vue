f58d9065dd9f8e226f7e18f9bd20eb8d867e50ff
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-13 19:25:17 +0300

    props in routes. passing name from welcome to chat component

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -6,6 +6,9 @@
                 <form @submit.prevent="enterChat">
                     <label for="name">Enter your name:</label>
                     <input type="text" name="name" v-model="name" />
+
+                    <p v-if="feedback" class="red-text">{{ feedback }}</p>
+
                     <button class="btn teal">Enter Chat</button>
                 </form>
             </div>
@@ -18,13 +21,18 @@ export default {
   name: 'Welcome',
   data () {
     return {
-        name: null
+        name: null,
+        feedback: null
     }
   },
 
   methods: {
       enterChat () {
-
+          if (this.name) {
+              this.$router.push({ name: 'Chat', params: { name: this.name } })
+          } else {
+              this.feedback = 'You must enter a name to join'
+          }
       }
   }
 }
