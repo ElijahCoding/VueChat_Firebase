f858dc035be7d3ddabeaeadd4000f778e856fa44
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-13 19:03:29 +0300

    welcome component styling

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -24,7 +24,7 @@ export default {
 
   methods: {
       enterChat () {
-          
+
       }
   }
 }
@@ -32,5 +32,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    .welcome {
+        max-width: 400px;
+        margin-top: 100px;
+    }
 
+    .welcome h2 {
+        font-size: 3em;
+    }
+
+    .welcome button {
+        margin: 30px auto;
+    }
 </style>
