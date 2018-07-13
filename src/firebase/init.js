c8da6ace8c5d6c52a1d47473e8fdb8070329f1d0
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-13 18:53:14 +0300

    firebase and its configuration set up

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+import firestore from 'firebase/store'
+
+var config = {
+    apiKey: "AIzaSyAegq8omGFF5aZwFGx8VvhnIIOQGsTCG5I",
+    authDomain: "vuefs-prod-36747.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-36747.firebaseio.com",
+    projectId: "vuefs-prod-36747",
+    storageBucket: "vuefs-prod-36747.appspot.com",
+    messagingSenderId: "850166238486"
+  };
+
+const firebaseApp = firebase.initializeApp(config);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebaseApp.firestore()
